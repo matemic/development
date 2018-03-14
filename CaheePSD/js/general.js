2e7fe9765a0a2cd4dada424fa07b2c854d8d68c8
Matisq <mnamtar@gmail.com>
2018-03-14 20:41:32 +0100

    Big update with responsive sections, smooth scroll, animations and transitions

--- CaheePSD/js/general.js
+++ CaheePSD/js/general.js
@@ -1,17 +1,16 @@
-$(document).ready(function()  {
-const element = function(el, heightContainer) {
-    return $(el).click(function(e) {
-        let navHeight =  $(heightContainer).outerHeight();
-        let hash = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top - navHeight
-        }
-    , 1000);
-        
-        e.preventDefault();
-    });
-}
+$(document).ready(function () {
+    const element = function (el, heightContainer) {
+        let navHeight = $(heightContainer).outerHeight();
+        return $(el).click(function (e) {
+            let hash = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000);
+            e.preventDefault();
+        })
+    }
 
-element('.nav-link', '.navigation');
-element('.scroll-down-link', '.navigation');
+    element('.scroll-down-link', '.navbar');
+    element('.header a', '.navbar');
+    setTimeout(() => element('.nav-link', '.navbar'), 500);
 });
