b11f7d977850d2b50a61a1ad41a3b67bb0a6f7b0
Matisq <mnamtar@gmail.com>
2018-05-27 22:06:38 +0200

    Added possiblity to close menu by clicking anywhere

--- CaheePSD/js/general.js
+++ CaheePSD/js/general.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
     const element = function (el, heightContainer) {
         let navHeight = $(heightContainer).outerHeight();
-        return $(el).click(function (e) {
+        $(el).click(function (e) {
             let hash = $(this).attr('href');
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 1000);
             e.preventDefault();
         })
+        $(window).click(function() {
+            if ( $('.navbar-collapse').is('.collapse.show')) {
+            $('.navbar-collapse').removeClass('show');
+            }
+        })
     }
 
     element('.scroll-down-link', '.navbar');
     element('.header a', '.navbar');
-    setTimeout(() => element('.nav-link', '.navbar'), 500);
+    element('.nav-link', '.navbar');
+    
 });
